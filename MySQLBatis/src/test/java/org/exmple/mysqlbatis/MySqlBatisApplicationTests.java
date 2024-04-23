fbc1ac41d5f011437b62360d4392ed6e4d525be1
Charles Chai <XF953987349@163.com>
2024-04-23 12:40:38 +0800

    新添加了基础的登录功能, 连接了专属数据库

--- MySQLBatis/src/test/java/org/exmple/mysqlbatis/MySqlBatisApplicationTests.java
+++ MySQLBatis/src/test/java/org/exmple/mysqlbatis/MySqlBatisApplicationTests.java
@@ -20,17 +20,6 @@ class MySqlBatisApplicationTests {
 
     @Test
    public void testUser() {
-       /* List<Integer> aa= Arrays.asList(1,2,3);
-        userMapper.deleteUser(aa);*/
-/*        List<User> userList = new ArrayList<>();
-        userList.add(new User(1, "Alice", 25));
-        userList.add(new User(2, "Bob", 30));
-        userList.add(new User(3, "Charlie", 28));
-        userMapper.createUserAccount(userList);*/
-        List<Employee> aa=employeeMapper.searchEmployee(null,null,null,null);
-        for (Employee employee : aa) {
-            System.out.println(employee);
-        }
     }
 
 }
