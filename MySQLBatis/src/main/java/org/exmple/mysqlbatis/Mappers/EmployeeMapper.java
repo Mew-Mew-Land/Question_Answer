70aebde5af8c2e9f448cd5a4050450718064c358
Charles Chai <XF953987349@163.com>
2024-04-23 12:43:17 +0800

    新添加了基础的登录功能, 连接了专属数据库

--- MySQLBatis/src/main/java/org/exmple/mysqlbatis/Mappers/EmployeeMapper.java
+++ MySQLBatis/src/main/java/org/exmple/mysqlbatis/Mappers/EmployeeMapper.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 @Mapper
 public interface EmployeeMapper {
-   /* List<Employee> employeeList();
-    List<Employee> searchEmployee(Integer ID, String name, Integer job, Character gender);*/
+    List<Employee> employeeList();
+    List<Employee> searchEmployee(Integer ID, String name, Integer job, Character gender);
     //注意int,char等这些是没有null(空值)的
     //为了实现实际查询条件为空的情况
     //需要使用包装类Integer,Character
