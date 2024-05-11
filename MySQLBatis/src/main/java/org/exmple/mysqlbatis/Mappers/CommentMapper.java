9684dc450226fd989e6902f03e6da0223650dcaa
Charles Chai <XF953987349@163.com>
2024-05-11 16:40:33 +0800

    自动配置数据库，加了很多（？）细节

---
+++ MySQLBatis/src/main/java/org/exmple/mysqlbatis/Mappers/CommentMapper.java
@@ -0,0 +1,7 @@
+package org.exmple.mysqlbatis.Mappers;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class CommentMapper {
+}
---
+++ MySQLBatis/src/main/java/org/exmple/mysqlbatis/service/CommentServant.java
@@ -0,0 +1,4 @@
+package org.exmple.mysqlbatis.service;
+
+public class CommentServant {
+}
